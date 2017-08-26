365f28766230473de21c309f36cff931e599c043
Vasilis Koukoutis <vkoukoutis@hotmail.com>
2017-08-26 22:51:39 +0300

    Fetching posts from wp test api. Still things to fix.

---
+++ index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>WpPostFetch</title>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 l-post-header">
+                <h3>Please type url of wordpress website you want to fetch posts.</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6 l-post">
+                <div class="input-group">
+                    <span class="input-group-addon" id="basic-addon3">https://</span>
+                    <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+
+                    <button type="button" class="btn btn-outline-light">Submit</button>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 wp-posts">
+                <h2>Recent posts</h2>
+                <ul>
+                    <?php
+
+                        $url          =  'https://demo.wp-api.org/wp-json/';
+                        $endpoint     =  'wp/v2/posts';
+                        $wpUrl        =  $url . $endpoint;
+                        $data         =  file_get_contents($wpUrl);
+                        $result       =  json_decode($data, true);
+                        $post_id      =  $result[0]['id'];
+                        $post_content =  $result[0]['content']['rendered'];
+
+                        if (empty($data)) {
+                            echo "<li><p>No posts found.</p></li>";
+                        } else {
+                            echo '<li><p>'.print_r($result, true ).'</p></li>';
+                        }
+                    ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
